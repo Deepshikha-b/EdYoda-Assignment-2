388cb8fc1dc1e32db0f7ede62686a82394fdf90d
Deepshikha <99143008+Deepshikha-b@users.noreply.github.com>
2022-05-22 23:03:30 +0530

    Update mini_dict.py

--- mini_dict.py
+++ mini_dict.py
@@ -1,10 +1,3 @@
-# my_dict = {}
- 
-# for i in range(97, 97 + 26):
-#     my_dict[chr(i)] = i
-#     print(my_dict)
- 
-# print(my_dict)
 keys = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 values = []
 for j in range(97,97+26):
